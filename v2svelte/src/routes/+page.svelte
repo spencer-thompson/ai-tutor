846d93ada2ab67c927e1dbd22c194d1ad6057964
Guts <gutsw20@gmail.com>
2025-01-15 15:24:38 -0700

    Shouldn't be too hard to add markdown

--- v2svelte/src/routes/+page.svelte
+++ v2svelte/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { SendHorizontal } from 'lucide-svelte';
 	import { resize } from '$lib/components/textarea/resize';
+	import { marked } from 'marked';
 	let name = 'textarea',
 		textarea = '',
 		height = 120,
@@ -28,15 +30,13 @@
 		message: string;
 	}
 
-	let count: number[] = [0];
-	let i = 1;
-
-	let messages: Message[] = [{ sender: Sender.user, message: 'this is a message from the user!' }];
+	let messages: Message[] = [
+		{ sender: Sender.user, message: 'this is a message from the user!' },
+		{ sender: Sender.ai, message: marked.parse('# Marked in **the** *brower*') }
+	];
 
 	function addMessage(sender: Sender, text: string) {
 		messages = [...messages, { sender, message: text }];
-		count.push(i);
-		i += 1;
 		console.log(text);
 		setTimeout(() => {
 			window.scrollTo(0, document.body.scrollHeight);
@@ -45,32 +45,32 @@
 
 	//This is the next part I am working on:
 	async function sendMessage(sender: Sender, text: string) {
-		addMessage(Sender.user, value);
+		if (text != '') addMessage(Sender.user, value);
 		value = '';
 
-		const courses = [101, 202];
-
-		let requestBody = {
-			messages: messages,
-			courses: courses
-		};
-
-		const response = await fetch('https://api.aitutor.live/v1/smart_chat_stream', {
-			method: 'POST',
-			body: JSON.stringify({
-				userId: 1,
-				title: 'I guess stuff goes here',
-				completed: false
-			}),
-			headers: {
-				'Content-Type': 'application/json',
-				'AITUTOR-API-KEY': '${headerManager.apiKey}',
-				Authorization: 'Bearer ${headerManager.jwt}'
-			}
-		});
-		for await (const chunk of response.body) {
-			console.log(chunk);
-		}
+		// const courses = [101, 202];
+		//
+		// let requestBody = {
+		// 	messages: messages,
+		// 	courses: courses
+		// };
+		//
+		// const response = await fetch('https://api.aitutor.live/v1/smart_chat_stream', {
+		// 	method: 'POST',
+		// 	body: JSON.stringify({
+		// 		userId: 1,
+		// 		title: 'I guess stuff goes here',
+		// 		completed: false
+		// 	}),
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 		'AITUTOR-API-KEY': '${headerManager.apiKey}',
+		// 		Authorization: 'Bearer ${headerManager.jwt}'
+		// 	}
+		// });
+		// for await (const chunk of response.body) {
+		// 	console.log(chunk);
+		// }
 	}
 
 	//check out basic_chat_ui.dart in playground!!!
@@ -84,7 +84,7 @@
 				<div class="flex items-start gap-2.5 mb-4">
 					<div
 						class="
-                    max-w-[220px] p-3 rounded-lg {message.sender === Sender.user
+                    max-w-[220px] py-1 px-3 rounded-lg {message.sender === Sender.user
 							? ' ml-auto bg-blue-600 rounded-br-none'
 							: ' mr-auto bg-gray-300 rounded-bl-none'}
                             "
@@ -94,8 +94,7 @@
 								? 'text-sm font-normal py-2.5 text-gray-900 dark:text-white'
 								: 'text-sm font-normal py-2.5 text-gray-900 dark:text-black'}
 						>
-							{message.message}
-							{count}
+							{@html message.message}
 						</p>
 					</div>
 				</div>
@@ -105,7 +104,7 @@
 
 	<div class="fixed gap-2 inset-x-0 bottom-10 mx-4">
 		<div class="max-w-2xl mx-auto">
-			<div class="bg-gray-100 rounded-lg p-3 relative">
+			<div class="bg-gray-100 rounded-3xl py-3 relative">
 				<div class="flex flex-col w-full pr-24">
 					<textarea
 						{rows}
@@ -119,8 +118,8 @@
 					></textarea>
 					<button
 						on:click={() => sendMessage(Sender.user, textarea.value)}
-						class=" absolute bottom-3 right-3 max-h-14 px-4 py-2 bg-blue-500 text-white rounded-xl focus:bg-blue-600"
-						>Click Here</button
+						class=" absolute bottom-5 right-3 max-h-14 px-4 py-2 bg-blue-500 text-white rounded-xl focus:bg-blue-600"
+						><SendHorizontal size="24" /></button
 					>
 				</div>
 			</div>
