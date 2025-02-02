23415a9fd0a7e90a3e2a4f52eadf191a4a5907ac
Guts <gutsw20@gmail.com>
2025-02-01 18:57:43 -0700

    started messing with katex

---
+++ svelte/src/routes/test_1/katex.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import katex from 'katex';
+	import 'katex/dist/katex.min.css';
+
+	export let math = '';
+	export let displayMode = false;
+
+	let element;
+
+	$: {
+		if (element) {
+			katex.render(math, element, {
+				throwOnError: false,
+				displayMode: displayMode
+			});
+		}
+	}
+</script>
+
+<span bind:this={element}></span>
