191906d63d1a38322f684c0e0e12aa9ff657b29b
Guts <gutsw20@gmail.com>
2025-04-24 07:22:02 -0600

    Was able to correctly display the courses, now need to make them toggleable

--- svelte/src/routes/CourseSettingsModal.svelte
+++ svelte/src/routes/CourseSettingsModal.svelte
@@ -5,41 +5,50 @@
 	import { userData } from '$lib/stores/userDataStore';
 
 	export let parent: SvelteComponent;
-	export let data;
 	const modalStore = getModalStore();
 	let color = 'red';
 
-	let flavors: Record<string, boolean> = {
-		course1: true,
-		course2: false,
-		course3: false,
-		course4: true,
-		course5: false,
-		course6: true,
-		course7: false
-	};
+	let course_booleans: Record<string, boolean> = {};
 
-	function toggle(flavor: string): void {
-		flavors[flavor] = !flavors[flavor];
+	const regex = /^([^-\s]+-[^-\s]+)(?:-.*)?$/;
+	let courses = $userData.courses;
+	for (let i = 0; i < courses.length; i++) {
+		const match = courses[i].name.match(regex);
+		if (match && match[1]) {
+			console.log(match[1]);
+			course_booleans[match[1]] = true;
+		} else {
+			console.log(courses[i].name);
+			course_booleans[courses[i].name] = true;
+		}
+	}
+
+	// console.log($userData.courses.length);
+	// JSON.stringify($userData, null, 2)
+
+	function toggle(courseName: string): void {
+		course_booleans[courseName] = !course_booleans[courseName];
 	}
 </script>
 
 {#if $modalStore[0]}
-	<pre>{JSON.stringify($userData, null, 2)}</pre>
+	<!-- {courses} -->
+	<!-- <pre>{JSON.stringify($userData.courses, null, 2)}</pre> -->
+	<!-- <pre>{JSON.stringify($userData, null, 2)}</pre> -->
 	<!-- {JSON.stringify(., null, 2)} -->
 	<div class="card p-4 w-modal shadow-xl space-y-4">
 		<header class="text-2xl font-bold">Select Courses</header>
 		<!--<button type="button" class="btn variant-filled"> Course1</button>-->
 		<div class="flex flex-wrap gap-3">
-			{#each Object.keys(flavors) as f}
+			{#each Object.keys(course_booleans) as f}
 				<button
-					class="btn {flavors[f] ? 'variant-filled' : 'variant-soft'}"
+					class="btn {course_booleans[f] ? 'variant-filled' : 'variant-soft'}"
 					on:click={() => {
 						toggle(f);
 					}}
 					on:keypress
 				>
-					{#if flavors[f]}<span><Check size="16" /></span>{/if}
+					{#if course_booleans[f]}<span><Check size="16" /></span>{/if}
 					<span class="capitalize">{f}</span>
 				</button>
 			{/each}
